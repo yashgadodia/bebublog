547d30716ebee98bcf682b0e4a3284d078a99888
Yash Gadodia <pirsquare.yash@gmail.com>
2020-04-24 17:05:48 +0800

    add aboutme controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'about', to: "pages#about" #get about to pages controller about action
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
