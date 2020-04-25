dc911f501a96d5a1fe707fba2dc0721b3cb1fad5
Yash Gadodia <pirsquare.yash@gmail.com>
2020-04-25 08:44:22 +0800

    WIP navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'pages#about'
   get 'about', to: "pages#about" #get about to pages controller about action
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
