40bf9d9c5ea8292401ec7ffe77fc8b3d29b2e729
Yash Gadodia <pirsquare.yash@gmail.com>
2020-04-26 22:08:30 +0800

    navbar complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#about'
   get 'about', to: "pages#about" #get about to pages controller about action
+  
+  # resources :posts
+  # get '/blog', to: 'posts#index'
+  # devise_for :users
+  # root 'posts#index'
+  # get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
