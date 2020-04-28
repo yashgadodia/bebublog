55b1b62740b6f6f87801662fefa0d971bff47937
Yash Gadodia <pirsquare.yash@gmail.com>
2020-04-28 09:58:25 +0800

    add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.all.order("created_at DESC").paginate(page: params[:page])
   end
 
   # GET /posts/1
