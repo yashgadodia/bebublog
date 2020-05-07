e5ffed701b04fb4a67c1643ec157990f1c233821
Yash Gadodia <pirsquare.yash@gmail.com>
2020-05-07 21:51:53 +0800

    change db to pg because heroku doesnt use sqlite

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   get 'about', to: "pages#about" #get about to pages controller about action
-<<<<<<< HEAD
-  
-  resources :comments
-=======
   get 'whatisthis', to: "pages#whatisthis" #get about to pages controller about action
->>>>>>> origin/working
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
