fc436145e6d4e88ed62d5d7b3891dabaf42812e9
Yash Gadodia <pirsquare.yash@gmail.com>
2020-05-25 16:35:12 +0800

    about me page

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,3 +1,12 @@
 document.addEventListener('turbolinks:load', function() {
   gtag('config', 'UA-167308723-1')
 });
+
+
+// document.addEventListener('turbolinks:load', function(event) {
+//   if (typeof gtag === 'function') {
+//     gtag('config', '<%= Rails.application.credentials.dig(:google_analytics) %>', {
+//       'page_location': event.data.url
+//     })
+//   }
+// })
